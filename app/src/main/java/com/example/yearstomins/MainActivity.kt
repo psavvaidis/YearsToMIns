f4d9b18356ca9dd3678829ecd5e932f061b7a3f2
psavvaidis <panagiotissav@gmail.com>
2020-07-28 00:27:47 +0300

    Adding Toast to "Calculate" button

--- app/src/main/java/com/example/yearstomins/MainActivity.kt
+++ app/src/main/java/com/example/yearstomins/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.yearstomins
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn_to_calc = findViewById<Button>(R.id.calc_btn) as Button
+
+        btn_to_calc.setOnClickListener{
+            Toast.makeText(this@MainActivity, "Calculation Complete", Toast.LENGTH_SHORT).show()
+
+        }
+
     }
 }
